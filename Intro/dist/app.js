e03166afca100bc947fd144473ca893ce9335654
Suraj Verma <kumarsurajverma6001@gmail.ccom>
2025-12-06 03:11:25 +0530

    Fix gitignore

--- Intro/dist/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-function greet(person) {
-  return `Hello ${person} , Welcome`;
-}
-const username = 'Suraj Verma';
-console.log(greet(username));
-//# sourceMappingURL=app.js.map
