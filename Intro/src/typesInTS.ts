d2dd1e2658aa295b198fdd13574f68034447b23d
Suraj Verma <kumarsurajverma6001@gmail.ccom>
2025-12-07 01:14:41 +0530

    Day 03

---
+++ Intro/src/typesInTS.ts
@@ -0,0 +1,11 @@
+let name = "Suraj Verma";
+
+let val = Math.random() > 0.5 ? 10 : 5;
+
+let userName = "vermasuraj";
+
+let favSports:string = "Cricket";
+
+favSports = "Football";
+
+let isCheck:boolean
